749a5cdf97fe90544db71e7a4d10145dd0bf93e4
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-01-01 22:12:06 +0200

    Add project files.

---
+++ NumbersN1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumbersN1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = int.Parse(Console.ReadLine()); i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ VowelsSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace VowelsSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int letterValue = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char letter = text[i];
+
+                switch (letter)
+                {
+                    case 'a':
+                        letterValue += 1;
+                        break;
+                    case 'e':
+                        letterValue += 2;
+                        break;
+                    case 'i':
+                        letterValue += 3;
+                        break;
+                    case 'o':
+                        letterValue += 4;
+                        break;
+                    case 'u':
+                        letterValue += 5;
+                        break;
+                }
+            }
+            Console.WriteLine(letterValue);
+        }
+    }
+}
